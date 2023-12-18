b8df03d1b5df7688568ccccbc586042109f5af57
Ali Hassan <86561709+DeathRiderJr@users.noreply.github.com>
2023-12-18 15:43:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Content Writer","Blogger"],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
